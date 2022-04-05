386f8ed2196204b7ec0caea80b4b358592a5ba6b
Tasneem Titus <titustasneem5@gmail.com>
2022-04-05 10:54:24 +0200

    new files

---
+++ myWebPage/myWebPage.js
@@ -0,0 +1,7 @@
+//    Navigation Bar
+function toggleMenu() {
+  let menuToggle = document.querySelector(".toggle");
+  let menu = document.querySelector(".menu");
+  menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
+}
